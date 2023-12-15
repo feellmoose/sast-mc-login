42b4f522b26097edb10205595e1134fb67519f83
FeelMoose <feell4429@163.com>
2023-12-15 12:46:22 +0800

    init

---
+++ src/main/java/fun/sast/sastlogin/Sast_login.java
@@ -0,0 +1,13 @@
+package fun.sast.sastlogin;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Sast_login implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/fun/sast/sastlogin/client/Sast_loginClient.java
@@ -0,0 +1,13 @@
+package fun.sast.sastlogin.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Sast_loginClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
